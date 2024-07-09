8303f90acd68b87505d004109067ac2f5230108e
anton572 <135094776+anton572@users.noreply.github.com>
2024-07-09 20:12:05 +0300

    none
    none

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 class main(App):
-    pass
+    def build(self):
+        super().build()
+        layout = BoxLayout(padding=10)
+        layout.add_widget(Button(text='des'))
+        layout.add_widget(Button(text='res'))
+        #B.bind(on_press=lambda a:print(5))
+        return layout
 if __name__ == '__main__':
     main().run()
 
